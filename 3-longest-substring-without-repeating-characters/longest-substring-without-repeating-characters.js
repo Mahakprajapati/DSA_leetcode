0c23bd909de37ba27b2014df66ac2ce97e0eda84
Mahakprajapati <mahakprajapati227@gmail.com>
2026-08-14 22:37:52 +0530

    Time: 66 ms (27.59%) | Memory: 60.4 MB (32.85%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let maxlen = 0
+    let map = new Map();
+
+    let i = 0, j = 0;
+    let n = s.length;
+
+    while (j < n) {
+
+        if (!map.has(s[j])) {
+            map.set(s[j], j);
+
+        } else {
+            if (map.get(s[j]) >= i && map.get(s[j]) <= j) {
+                i = map.get(s[j]) + 1;
+                map.set(s[j], j);
+            }
+        }
+         map.set(s[j], j);
+        maxlen = Math.max(maxlen, j - i + 1);
+        j++;
+    }
+
+    return maxlen;
+};
